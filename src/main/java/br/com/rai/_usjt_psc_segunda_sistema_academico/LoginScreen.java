609a35bd21d05e80fb9f050545ecf4f716c8a79b
Raí Joia <megac154@gmail.com>
2023-10-02 21:46:36 -0300

    ap11(teste): screen of login implement

--- src/main/java/br/com/rai/_usjt_psc_segunda_sistema_academico/LoginScreen.java
+++ src/main/java/br/com/rai/_usjt_psc_segunda_sistema_academico/LoginScreen.java
@@ -4,6 +4,8 @@
  */
 package br.com.rai._usjt_psc_segunda_sistema_academico;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 82318841
@@ -26,8 +28,16 @@ public class LoginScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
         loginTextField = new javax.swing.JTextField();
         senhaPasswordField = new javax.swing.JPasswordField();
+        okLoginButton = new javax.swing.JButton();
+        cancelarLoginButton = new javax.swing.JButton();
+
+        jButton1.setText("jButton1");
+
+        jButton2.setText("jButton2");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,25 +55,44 @@ public class LoginScreen extends javax.swing.JFrame {
             }
         });
 
+        okLoginButton.setText("Ok");
+        okLoginButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                okLoginButtonActionPerformed(evt);
+            }
+        });
+
+        cancelarLoginButton.setText("Cancelar");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(137, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(loginTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 192, Short.MAX_VALUE)
-                    .addComponent(senhaPasswordField))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(okLoginButton)
+                        .addGap(18, 18, 18)
+                        .addComponent(cancelarLoginButton))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(loginTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 192, Short.MAX_VALUE)
+                        .addComponent(senhaPasswordField)))
                 .addGap(136, 136, 136))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(150, Short.MAX_VALUE)
-                .addComponent(loginTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(137, Short.MAX_VALUE)
+                .addComponent(loginTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(senhaPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(164, 164, 164))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(okLoginButton)
+                    .addComponent(cancelarLoginButton))
+                .addGap(129, 129, 129))
         );
 
         pack();
@@ -76,6 +105,25 @@ public class LoginScreen extends javax.swing.JFrame {
     private void senhaPasswordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_senhaPasswordFieldActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_senhaPasswordFieldActionPerformed
+
+    private void okLoginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okLoginButtonActionPerformed
+        // passo 1 pegar login 
+        // getText pega o texto que o usuário digitou
+        String login = loginTextField.getText();
+        
+        // passo 2 pegar a senha
+        String senha = new String(senhaPasswordField.getPassword());
+        
+        // passo 3 verificar se ambos são admin
+        if (login.equals("admin") && senha.equals("admin")) {
+            // se for, dar boas vindas
+            JOptionPane.showMessageDialog(null, "Bem-Vindo!");
+        } else {
+            // se não for, informar chave/valor incorreto
+            JOptionPane.showMessageDialog(null, "Login e/ou Senha está inválido");
+        }
+        
+    }//GEN-LAST:event_okLoginButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -113,7 +161,11 @@ public class LoginScreen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cancelarLoginButton;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JTextField loginTextField;
+    private javax.swing.JButton okLoginButton;
     private javax.swing.JPasswordField senhaPasswordField;
     // End of variables declaration//GEN-END:variables
 }
